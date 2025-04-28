852530d957573266a4bfd42f5701d776540fa746
23407je <23407je@hvhs.school.nz>
2025-04-28 21:51:58 +0000

    Apr 28, 2025, 9:51 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,7 +1,28 @@
 /****************************
+ 
 KeyToPage Josh 29/04/25
 ****************************/
 console.log("Running t01_introduction.js");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
+console.log("Coby");
 
 //variables
 
