5f844a143f620d7836bc877795d3143efb76ffc5
23407je <23407je@hvhs.school.nz>
2025-04-30 00:18:23 +0000

    Apr 30, 2025, 12:18 AM

---
+++ t03_maths.js
@@ -0,0 +1,26 @@
+/****************************
+ 
+KeyToPage Josh 30/04/25
+****************************/
+
+//variables
+console.log(1+2)
+
+var mynumber = 5;
+console.log(mynumber + 3)
+
+var myothernumber = 10;
+console.log(mynumber = myothernumber);
+
+
+
+/****************************
+ Main code
+****************************/
+
+
+
+
+/****************************
+  Functions
+****************************/
