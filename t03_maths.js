240d98d7b461879a07c870a87f2e25908a2b0b90
23407je <23407je@hvhs.school.nz>
2025-04-30 00:21:34 +0000

    Apr 30, 2025, 12:21 AM

--- t03_maths.js
+++ t03_maths.js
@@ -4,14 +4,15 @@ KeyToPage Josh 30/04/25
 ****************************/
 
 //variables
-console.log(1+2)
 
 var mynumber = 5;
-console.log(mynumber + 3)
 
 var myothernumber = 10;
-console.log(mynumber + myothernumber);
 
+console.log("add", mynumber + myothernumber)
+console.log("add", mynumber - myothernumber)
+console.log("add", mynumber * myothernumber)
+console.log("add", mynumber / myothernumber)
 
 
 /****************************
