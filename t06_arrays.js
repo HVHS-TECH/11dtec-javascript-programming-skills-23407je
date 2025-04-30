e4196f8f4f0517c97502a5f7dff0bd44d9c239bc
23407je <23407je@hvhs.school.nz>
2025-04-30 07:20:39 +0000

    Apr 30, 2025, 7:20 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -1,2 +1,4 @@
-var numberArray = ["1","2","3","4","5","6",];
-alert=("pick a number from this set" +numberArray+);
+alert=("Welcome to my program");
+var age = prompt("Welcome to my program \nWhat is your name?");
+var username = prompt("Welcome to my program " +age+ "\nWhat is your age?");
+var username = prompt("You are fucked now and we will find your profiles and hack them with you're age of " +username+ " and name of " +age+ "\nThanks this was a stall you're computer now has a bitcoin miner and cookie grabber that I installed on it but no antivirus will help and they wont detect it");
