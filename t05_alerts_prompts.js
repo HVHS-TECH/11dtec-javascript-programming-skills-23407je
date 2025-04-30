622b7a019a41c2dfdf43f164a906f56548f32879
23407je <23407je@hvhs.school.nz>
2025-04-30 06:41:50 +0000

    Apr 30, 2025, 6:41 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -1,2 +1,4 @@
 alert=("Welcome to my program");
-var username = prompt("Welcome to my program " +username+ "\nWhat is your name?");
+var username = prompt("Welcome to my program \nWhat is your name?");
+var username = prompt("Welcome to my program " +username+ "\nWhat is your age?");
+var username = prompt("You are fucked now that I have your age of " +username+ "\nThanks this was a stall you're computer now has a bitcoin miner I installed on it");
