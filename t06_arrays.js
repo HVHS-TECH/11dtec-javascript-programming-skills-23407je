27a60a797725095416518d14e337c2e0d6cfc52c
23407je <23407je@hvhs.school.nz>
2025-05-01 00:39:42 +0000

    May 1, 2025, 12:39 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -1,5 +1,2 @@
-let nameArray = ["lukas","Josh","Pasha","Coby","Seth","Jahlayze"];
-alert=("Pick a number 1-6 and it will display one of these names lukas,Josh,Pasha,Coby,Seth,Jahlayze");
-var Number = prompt("Welcome to my program \nPick a number from 1-6 and it will display one of these names lukas,Josh,Pasha,Coby,Seth,Jahlayze");
-alert("person " +number+ "is" +nameArray[number])
-var username = prompt("You are fucked now and we will find your profiles and hack them with you're age of " +username+ " and name of " +age+ "\nThanks this was a stall you're computer now has a bitcoin miner and cookie grabber that I installed on it but no antivirus will help and they wont detect it");
+var classarray = ["jake","james","jarvis","jamal","jane","coby",];
+let choice = prompt("enter a number from 1 to 6")
