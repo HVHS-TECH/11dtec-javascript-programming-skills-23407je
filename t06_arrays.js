4e00a339b88968ffcdd058aca2396e8fe94e0285
23407je <23407je@hvhs.school.nz>
2025-05-01 00:44:19 +0000

    May 1, 2025, 12:44 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -1,3 +1,3 @@
 var classArray = ["jake","james","jarvis","jamal","jane","coby",];
-let choice = prompt("enter a number from 1 to 6");
-alert("person" +choice+ "is" +classArray[choice]);
+let choice = prompt("enter a number from 0 to 5");
+alert("person " +choice+ " is " +classArray[choice]);
