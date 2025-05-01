7251a01806dc4bd02bb2fcccb32184ca35054433
23407je <23407je@hvhs.school.nz>
2025-05-01 01:23:23 +0000

    May 1, 2025, 1:23 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -1,4 +1,23 @@
-alert=("Welcome to my program");
+
+/****************************
+ 
+KeyToPage Josh 29/04/25
+****************************/
+console.log("Running t01_introduction.js");
+console.log("Coby - Lukas 29/04/25");
+
+//variables
 var age = prompt("Welcome to my program \nWhat is your name?");
 var username = prompt("Welcome to my program " +age+ "\nWhat is your age?");
 var username = prompt("You are fucked now and we will find your profiles and hack them with you're age of " +username+ " and name of " +age+ "\nThanks this was a stall you're computer now has a bitcoin miner and cookie grabber that I installed on it but no antivirus will help and they wont detect it");
+
+/****************************
+ Main code
+****************************/
+alert=("Welcome to my program");
+
+
+
+/****************************
+  Functions
+****************************/
