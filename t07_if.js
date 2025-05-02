a91116cbe3fbfefe3cb010daefb3ea07a7f9ee92
23407je <23407je@hvhs.school.nz>
2025-05-02 02:29:57 +0000

    May 2, 2025, 2:29 AM

--- t07_if.js
+++ t07_if.js
@@ -17,7 +17,7 @@ var ratedG = prompt("you are not able to see an X-rated movies");
 }
 var name = prompt("what is your name?");
  if (age >= "18") {
-      var xrated = prompt("you are to old for me as you are " +age+ +name+ " please dont visit again");
+      var xrated = prompt("you are to old for me as you are " +age+ "ugly name of " +name+ " please dont visit again");
 } else {
 var ratedG = prompt("you are the perfect age of " +age+ " come here " +name+ " I have a huge suprise for you");
 }
