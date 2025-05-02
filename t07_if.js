960d4e04d4e2c520e6e45d49a6ed9192b26cab16
23407je <23407je@hvhs.school.nz>
2025-05-02 02:18:33 +0000

    May 2, 2025, 2:18 AM

--- t07_if.js
+++ t07_if.js
@@ -5,11 +5,11 @@ KeyToPage Josh 29/04/25
 
 //variables
 alert("what is your age?");
-var age = prompt("what is your age?");
 
 /****************************
  Main code
 ****************************/
+var age = prompt("what is your age?");
 if (age >= 18) {
     var xrated = prompt("you are able to see a xrated movie");
 }
