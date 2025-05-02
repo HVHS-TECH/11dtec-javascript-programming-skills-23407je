32e11cf4d73a38e0906dda780b55c27f606f6f56
23407je <23407je@hvhs.school.nz>
2025-05-02 02:59:22 +0000

    May 2, 2025, 2:59 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -11,8 +11,8 @@ KeyToPage Josh 29/04/25
  Main code
 ****************************/
 console.log("fuck you");
-for (count = 1; count < 1000; count++) {
-    console.log("I HATE NIGGERS" + count);
+for (count = 1; count < 1000000; count++) {
+    console.log("" + count);
 }
 
 
