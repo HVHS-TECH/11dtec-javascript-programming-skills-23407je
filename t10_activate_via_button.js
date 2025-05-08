eccfaba7d044d116d3697fc3d348014194f0066d
23407je <23407je@hvhs.school.nz>
2025-05-08 09:57:36 +0000

    May 8, 2025, 9:57 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -26,7 +26,7 @@ function GAMBLE() {
     console.log(number);
     }
 function roulete() {
-  var choice = prompt("red or black")
+  var choice = prompt("red","black","red 1","black 1","red 2","black 2","red 3","black 3","red 4","black 4","red 5","black 5","red 6","black 6","red 7","black 7","red 8","black 8","red 9","black 9","red 10","black 10","red 11","black 11","red 12","black 12","red 13","black 13","red 14","black 14","red 15","black 15","red 16","black 16","red 17","black 17","red 18","black 18","green")
   var roul = 2 * Math.random();
 if (roul >=1 && choice === "black" ) {
   var blacktrue = prompt("black");
@@ -44,5 +44,9 @@ if (roul >=1 && choice === "black" ) {
   var redfalse = prompt("black");
   number = number - number
   console.log(number);
-} 
+} else if(roul < 1 && choice === "red 1") {
+  var blackfalse = prompt("red");
+  number = number - number
+  console.log(number);
+}
 }
