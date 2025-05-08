cd453509abb44cc88b185e668919a341fb0ed42e
23407je <23407je@hvhs.school.nz>
2025-05-08 05:39:08 +0000

    May 8, 2025, 5:39 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -39,11 +39,6 @@ if (roul >=1 && classArray[black] ) {
   var black = prompt("black");
   number = number * 1.8
   console.log(number);
-  if (roul < 1 && classArray[black] ) {
-    var black = prompt("black");
-    number = 0;
-    console.log(number);
-  }
 } else {
   var black = prompt("black");
   number = 0;
