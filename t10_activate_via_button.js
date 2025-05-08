567c65591004232bcd78d537575a4f3503723644
23407je <23407je@hvhs.school.nz>
2025-05-08 10:05:15 +0000

    May 8, 2025, 10:05 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -26,7 +26,8 @@ function GAMBLE() {
     console.log(number);
     }
 function roulete() {
-  var choice = prompt("red","black","red 1","black 1","red 2","black 2","red 3","black 3","red 4","black 4","red 5","black 5","red 6","black 6","red 7","black 7","red 8","black 8","red 9","black 9","red 10","black 10","red 11","black 11","red 12","black 12","red 13","black 13","red 14","black 14","red 15","black 15","red 16","black 16","red 17","black 17","red 18","black 18","green")
+  var choice = prompt("red","black");
+  var chance =("non-existant","non-existant2","red","black","red 1","black 1","red 2","black 2","red 3","black 3","red 4","black 4","red 5","black 5","red 6","black 6","red 7","black 7","red 8","black 8","red 9","black 9","red 10","black 10","red 11","black 11","red 12","black 12","red 13","black 13","red 14","black 14","red 15","black 15","red 16","black 16","red 17","black 17","red 18","black 18","green")
   var roul = 2 * Math.random();
 if (roul >=1 && choice === "black" ) {
   var blacktrue = prompt("black");
@@ -44,20 +45,80 @@ if (roul >=1 && choice === "black" ) {
   var redfalse = prompt("black");
   number = number - number
   console.log(number);
-} else if(roul < 1 && choice === "red 1") {
+} else if(roul < 1 && chance === "red 1") {
   var blackfalse1 = prompt("red");
   number = number - number
   console.log(number);
-} else if(roul < 1 && choice === "red 2") {
+} else if(roul < 1 && chance === "red 2") {
   var blackfalse2 = prompt("red");
   number = number - number
   console.log(number);
-} else if(roul < 1 && choice === "red 3") {
+} else if(roul < 1 && chance === "red 3") {
   var blackfalse3 = prompt("red");
   number = number - number
   console.log(number);
-} else if(roul < 1 && choice === "red 4") {
+} else if(roul < 1 && chance === "red 4") {
   var blackfalse4 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 5") {
+  var blackfalse5 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 6") {
+  var blackfalse6 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 7") {
+  var blackfalse7 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 8") {
+  var blackfalse8 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 9") {
+  var blackfalse9 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 10") {
+  var blackfalse10 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 11") {
+  var blackfalse11 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 12") {
+  var blackfalse12 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 13") {
+  var blackfalse13 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 14") {
+  var blackfalse14 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 15") {
+  var blackfalse15 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 16") {
+  var blackfalse16 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 17") {
+  var blackfalse17 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "red 18") {
+  var blackfalse18 = prompt("red");
+  number = number - number
+  console.log(number);
+} else if(roul < 1 && chance === "green") {
+  var greenfalse = prompt("red");
   number = number - number
   console.log(number);
 }
