2aeb956665a94d1cdc51a8440ce2dbc8328bce03
23407je <23407je@hvhs.school.nz>
2025-05-16 02:25:23 +0000

    May 16, 2025, 2:25 AM

--- t14_constants.js
+++ t14_constants.js
@@ -11,8 +11,12 @@ var age;
 /****************************
  Main code
 ****************************/
-
-
+userage = prompt("what is your age?");
+if (userage >= drivingage) {
+  alert("you are old enough to drive");
+} else {
+  alert("you are not allowed to drive I will take you to")
+}
 
 
 /****************************
