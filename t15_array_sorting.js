0cf3d47eaa12ca82f400191fd45b61d27707b97b
23407je <23407je@hvhs.school.nz>
2025-05-26 21:10:48 +0000

    May 26, 2025, 9:10 PM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -6,7 +6,8 @@ KeyToPage Josh 29/04/25
 
 
 //variables
-let classarray = ["Mr.Goodyear", "Mr.Britton", "Mr.Williams", "Mr.Blomquist", "Mr."]
+let classarray = ["Mr.Goodyear", "Mr.Britton", "Mr.Williams", "Mr.Blomquist", "Mr.Gillies"];
+
 
 /****************************
  Main code
